12c8503112cf2847014eee22cc2eeb249c31f0ce
Alberto Schicht <alberto.schicht@joinmosaic.com>
2019-11-03 12:09:06 -0800

    abms terminados

--- tp_acp1/admin.py
+++ tp_acp1/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from tp_acp1.models import MedioDePago, Plato
+from tp_acp1.models import MedioDePago, Plato, Categoria, Filtro, MenuDelDia, Sugerencia, Promocion
 
 admin.site.register(MedioDePago)
 admin.site.register(Plato)
+admin.site.register(Categoria)
+admin.site.register(Filtro)
+admin.site.register(MenuDelDia)
+admin.site.register(Promocion)
+admin.site.register(Sugerencia)
